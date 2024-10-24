ac01effdbbb80f7ff90b5febd9ee869561b8c1b8
Jerry Chen <jerryvchen1@gmail.com>
2024-10-23 22:39:21 -0700

    adjusted home centering and margins

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ import './Home.scss';
 const Home: React.FC = () => {
     return (
         <div className="container home-container">
-            <div className="row align-items-center">
-                <div className="col-md-6">
+            <div className="row justify-content-center">
+                <div className="col-md-6 home-text">
                     <h1>Hello</h1>
                     <p>This is Jerry Chen</p>
                 </div>
