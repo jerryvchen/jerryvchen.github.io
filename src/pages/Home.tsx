86f069dee56a82bcffd0da5e02c2ee587cacb011
Jerry Chen <jerryvchen1@gmail.com>
2024-11-04 21:12:19 -0800

    updated home links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,11 +23,11 @@ const Home: React.FC = () => {
                 <ul className="positions-list">
                     <li><b>Student Researcher</b> - Vision Group @ UCI</li>
                     <ul>
-                        <li>Led by Alexander C. Berg and Charless C. Fowlkes</li>
+                        <li>Led by <a href="https://acberg.com/" target="_blank" rel="noopener noreferrer">Alexander C. Berg</a> and <a href="https://ics.uci.edu/~fowlkes/" target="_blank" rel="noopener noreferrer">Charless C. Fowlkes</a></li>
                     </ul>
                     <li><b>Treasurer</b> - ACM @ UCI</li>
                     <ul>
-                        <li>See https://www.acm-uci.org/</li>
+                        <li>See our club page <a href="https://www.acm-uci.org/" target="_blank" rel="noopener noreferrer">here</a>!</li>
                     </ul>
                 </ul>
             </div>
